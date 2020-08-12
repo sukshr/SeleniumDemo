5379ac2cefd329c8b094e7683ed978e98f297443
sukshr <sukritistha@gmail.com>
2020-08-11 19:15:06 -0500

    used annotation "@FindBy" to find the element instead of finding elements using "findelement".

--- src/automatedTesting/automatedTesting/PINPage.java
+++ src/automatedTesting/automatedTesting/PINPage.java
@@ -1,52 +1,63 @@
 package automatedTesting.automatedTesting;
 
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 public class PINPage {
-WebDriver driver;
+	WebDriver driver;
+	@FindBy(id = "ctl00_cphStepPageData_txtPIN")
+	WebElement PINNumber;
+	@FindBy(id = "ctl00_cphStepPageData_txtSSN4")
+	WebElement SSNNumber;
+	@FindBy(id = "ctl00_cphStepPageData_ucDOB_ddlMonth")
+	WebElement Month;
+	@FindBy(id = "ctl00_cphStepPageData_ucDOB_ddlDay")
+	WebElement Date;
+	@FindBy(id = "ctl00_cphStepPageData_ucDOB_ddlYear")
+	WebElement Year;
+	@FindBy(id = "ctl00_cphStepPageData_lblContinue")
+	WebElement Continue;
 
-public PINPage (WebDriver _driver) {
-	driver = _driver;
-	PageFactory.initElements(driver, this);
-}
-
-public PINPage pinnumber(String number) {
-	driver.findElement(By.id("ctl00_cphStepPageData_txtPIN")).sendKeys(number);
-	return this;
+	public PINPage(WebDriver _driver) {
+		driver = _driver;
+		PageFactory.initElements(driver, this);
 	}
 
-public PINPage ssnnumber(String nbr) {
-driver.findElement(By.id("ctl00_cphStepPageData_txtSSN4")).sendKeys(nbr);
-return this;
-}
+	public PINPage pinnumber(String number) {
+		PINNumber.sendKeys(number);
+		return this;
+	}
 
-public PINPage monthdropdown () {
-	WebElement dob = driver.findElement(By.id("ctl00_cphStepPageData_ucDOB_ddlMonth"));
-	Select dropdown = new Select(dob);
-			dropdown.selectByVisibleText("July");
-			return this;
-}
-public PINPage datedropdown () {
-	WebElement day = driver.findElement(By.id("ctl00_cphStepPageData_ucDOB_ddlDay"));
-	Select ddropdown = new Select(day);
-			ddropdown.selectByVisibleText("11");
-			return this;
-}
-public PINPage yeardropdown() {
-	WebElement year = driver.findElement(By.id("ctl00_cphStepPageData_ucDOB_ddlYear"));
-	Select ydropdown = new Select(year);
-			ydropdown.selectByVisibleText("2018");
-			return this;
-}
-public PINPage contirnuebtn() {
-driver.findElement(By.id("ctl00_cphStepPageData_lblContinue")).click();
-return this;
+	public PINPage ssnnumber(String nbr) {
+		SSNNumber.sendKeys(nbr);
+		return this;
+	}
 
-}
-}
+	public PINPage monthdropdown() {
+		Select dropdown = new Select(Month);
+		dropdown.selectByVisibleText("July");
+		return this;
+	}
 
+	public PINPage datedropdown() {
+		Select ddropdown = new Select(Date);
+		ddropdown.selectByVisibleText("11");
+		return this;
+	}
+
+	public PINPage yeardropdown() {
+		Select ydropdown = new Select(Year);
+		ydropdown.selectByVisibleText("2018");
+		return this;
+	}
+
+	public void contirnuebtn() {
+		Continue.click();
+	
+
+	}
+}
